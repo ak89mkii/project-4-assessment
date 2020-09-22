7470af5746b5d9015ca5be3db389a5dae47fd031
A.I. Kasem <ak89mkii@gmail.com>
2020-09-22 15:00:29 -0400

    Updates

---
+++ precache-manifest.66c39afdccddeb2fc2c64377602d06d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602242ea0cb6ee32aaf6f8b7d80b1e92",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "ef42aa1bb9bb0b97f7bf",
+    "url": "/project-4-assessment/static/css/main.48a7fa6c.chunk.css"
+  },
+  {
+    "revision": "5095fc7a733ed5461f57",
+    "url": "/project-4-assessment/static/js/2.e20c1975.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.e20c1975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef42aa1bb9bb0b97f7bf",
+    "url": "/project-4-assessment/static/js/main.4e42459d.chunk.js"
+  },
+  {
+    "revision": "d9abc4f48469944111a0",
+    "url": "/project-4-assessment/static/js/runtime-main.055201c0.js"
+  }
+]);
